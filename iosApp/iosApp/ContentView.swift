0477f4a5917b446f2a07262436c68ffb93bfb762
Victor Zambon <victorconforto@outlook.com>
2026-05-25 13:20:57 -0300

    Update ContentView.swift

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -4,9 +4,8 @@ import Shared
 struct RootView: View {
     @State private var isLoggedIn: Bool = false
     @State private var isChecking: Bool = true
-
     private let authRepository = KoinHelper.getAuthRepository()
-
+    
     var body: some View {
         Group {
             if isChecking {
@@ -26,17 +25,18 @@ struct RootView: View {
             isChecking = false
         }
     }
-    extension Color {
-        init(hex: String) {
-            let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
-            var int: UInt64 = 0
-            Scanner(string: hex).scanHexInt64(&int)
-            let a, r, g, b: UInt64
-            switch hex.count {
-            case 6: (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-            default: (a, r, g, b) = (1, 1, 1, 0)
-            }
-            self.init(.sRGB, red: Double(r)/255, green: Double(g)/255, blue: Double(b)/255, opacity: Double(a)/255)
+}
+
+extension Color {
+    init(hex: String) {
+        let hex = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int: UInt64 = 0
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 6: (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+        default: (a, r, g, b) = (1, 1, 1, 0)
         }
+        self.init(.sRGB, red: Double(r)/255, green: Double(g)/255, blue: Double(b)/255, opacity: Double(a)/255)
     }
 }
