9b384730cc1a7fa44a4fb55679d03bf0469f166d
joao.zambom <abutodzn@@gmail.com>
2026-05-25 14:18:43 -0300

    refactory ioskoin2

--- iosApp/iosApp/iOSApp.swift
+++ iosApp/iosApp/iOSApp.swift
@@ -1,16 +1,20 @@
 import SwiftUI
-import Shared
 
 @main
 struct iOSApp: App {
-
-    init() {
-        IosKoinInitKt.initKoin()
-    }
-
     var body: some Scene {
         WindowGroup {
-            RootView()
+            ZStack {
+                Color.black.ignoresSafeArea()
+                VStack(spacing: 20) {
+                    Text("RAYEARTH GAMES")
+                        .font(.largeTitle)
+                        .fontWeight(.black)
+                        .foregroundColor(Color(red: 0.91, green: 0.11, blue: 0.39))
+                    Text("iOS App funcionando!")
+                        .foregroundColor(.white)
+                }
+            }
         }
     }
 }
