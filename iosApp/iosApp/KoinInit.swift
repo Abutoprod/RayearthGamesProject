6bbfa45cff4a97a0b1edd72c1926529f6d650956
joao.zambom <abutodzn@@gmail.com>
2026-05-26 10:27:58 -0300

    excluding coin initconfig

--- iosApp/iosApp/KoinInit.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import Shared
-
-// Wrapper simples para inicializar o Koin
-// Evita crash na inicialização do app
-func initKoin() {
-    // O Koin é inicializado pelo módulo Kotlin via KoinInit.ios.kt
-    // que chama startKoin com iosModule + sharedModule
-}
